a023ab813cadfa05fd351d2afbff92be34048881
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2025-05-06 00:01:03 +0200

    Change header spacing

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Link } from "@tanstack/react-router";
 export default function Header() {
   return (
     <nav className="bg-secondary">
-      <ul className="flex gap-3">
+      <ul className="flex justify-between">
         <Link to="/">Home</Link>
         <Link to="/bot">Discord Bot</Link>
       </ul>
