abf1c1ead00459fa3036d5c896a22bc3f0d7813e
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2025-05-11 11:25:14 +0200

    Improve editor styling

--- src/components/FramedataNotes.tsx
+++ src/components/FramedataNotes.tsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import { Framedata } from "../__types/apiResponse";
 import FullScreenOverlay from "./FullScreenOverlay";
+import { SlOptionsVertical } from "react-icons/sl";
+import { MdDelete, MdEdit } from "react-icons/md";
+import { IoMdAdd } from "react-icons/io";
 
 type FramedataEditMenuProps = { framedata: Framedata };
 export default function FramedataEditMenu({
@@ -22,28 +25,36 @@ export default function FramedataEditMenu({
 
   return (
     <>
-      <div className="flex gap-1 h-fit relative">
+      <div className="h-fit relative [&_button]:w-8 [&_button]:h-8 [&_button]:flex [&_button]:items-center [&_button]:justify-center ">
         <div
-          className={`flex flex-col relative transition-all duration-200 z-10 ${showMenu ? "visible" : "hidden"} [&_button]:sticky`}
+          className={`flex flex-col relative transition-all ${showMenu && "[&_button]:drop-shadow-[0px_0px_10px_rgba(0,0,0,0.8)] [&_button]:opacity-100"} [&_button]:opacity-0`}
         >
           <button
             onClick={toggleEditInterface}
-            className={`bg-green-400 text-black p-2 rounded-full top-0`}
-          ></button>
+            className={`z-10 transition-all absolute bg-green-400 text-black rounded-full ${showMenu ? "-translate-x-14" : "translate-y-0 translate-x-0"}`}
+          >
+            <IoMdAdd />
+          </button>
           <button
             onClick={toggleEditInterface}
-            className={`bg-yellow-400 text-black p-2 rounded-full top-1/2 -translate-y-1/2`}
-          ></button>
+            className={`z-10 transition-all absolute bg-yellow-400 text-black rounded-full ${showMenu ? "translate-y-10 -translate-x-10" : "translate-y-0 translate-x-0"}`}
+          >
+            <MdEdit />
+          </button>
           <button
             onClick={toggleEditInterface}
-            className={`bg-red-400 text-black p-2 rounded-full bottom-0`}
-          ></button>
+            className={`z-10 transition-all absolute bg-red-400 text-black rounded-full ${showMenu ? "translate-y-14" : "translate-y-0 translate-x-0"}`}
+          >
+            <MdDelete />
+          </button>
         </div>
         <div className="flex flex-col justify-center z-20">
           <button
             onClick={toggleMenu}
-            className="bg-blue-400 text-black p-2 rounded-full"
-          ></button>
+            className="bg-blue-400 text-black rounded-full z-20"
+          >
+            <SlOptionsVertical />
+          </button>
         </div>
       </div>
       {showInterfaceOverlay && (
