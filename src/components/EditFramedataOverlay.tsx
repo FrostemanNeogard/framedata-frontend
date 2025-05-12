6b0b6899bed5b69f09cab095466dffe1ceb7f507
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2025-05-12 22:47:10 +0200

    Begin creating "modify" suggestion creation

--- src/components/EditFramedataOverlay.tsx
+++ src/components/EditFramedataOverlay.tsx
@@ -1,5 +1,8 @@
+import { IoMdClose } from "react-icons/io";
 import { Framedata } from "../__types/apiResponse";
 import FullScreenOverlay from "./FullScreenOverlay";
+import { ChangeEvent, FormEvent, useState } from "react";
+import { toast } from "react-toastify";
 
 type EditFramedataOverlayProps = {
   framedata: Framedata;
@@ -9,67 +12,171 @@ export default function EditFramedataOverlay({
   framedata,
   callback,
 }: EditFramedataOverlayProps) {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const [input, setInput] = useState<string>(framedata.input);
+  const [hitLevel, setHitLevel] = useState<string>(framedata.hit_level);
+  const [damage, setDamage] = useState<string>(framedata.damage);
+  const [startup, setStartup] = useState<string>(framedata.startup);
+  const [hit, setHit] = useState<string>(framedata.hit);
+  const [block, setBlock] = useState<string>(framedata.block);
+  const [counter, setCounter] = useState<string>(framedata.counter);
+  const [notes, setNotes] = useState<string[]>(framedata.notes);
+
+  const modifyFramedata = async (e: FormEvent) => {
+    e.preventDefault();
+
+    const payload = {
+      action: "modify",
+      target: {
+        gameCode: "tekken8",
+        character: "alisa",
+        input: framedata.input,
+      },
+      payload: {
+        input,
+        hit_level: hitLevel,
+        damage,
+        startup,
+        hit,
+        block,
+        counter,
+        notes,
+      },
+    };
+
+    setIsLoading(true);
+    const response = await fetch(
+      `${import.meta.env.VITE_BASE_API_URL}suggestions`,
+      {
+        method: "POST",
+        body: JSON.stringify(payload),
+      }
+    );
+
+    if (response.status == 201) {
+      toast.success("Suggestion sent!");
+    } else {
+      toast.error("Something went wrong. Please try again later.");
+    }
+    setIsLoading(false);
+    callback();
+  };
+
+  const handleNoteChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    const value = e.target.value;
+    const parsedNotes = value.split("\n");
+    setNotes(parsedNotes);
+  };
+
   return (
     <FullScreenOverlay>
-      <button onClick={callback}>CLOSE</button>
-      <h1>Editing</h1>
-      <form
-        className={`grid [&_input]:text-secondaryText [&_textarea]:text-secondaryText`}
-      >
-        <label htmlFor="input">Input</label>
-        <input
-          type="text"
-          name="input"
-          id="input"
-          defaultValue={framedata.input}
-        />
+      <div className="flex flex-col gap-5">
+        <div className="flex justify-between">
+          <h1 className="text-xl">Suggest framedata entry modification</h1>
+          <button
+            onClick={callback}
+            className="p-2 bg-red-400 hover:bg-red-500 rounded text-black"
+            disabled={isLoading}
+          >
+            <IoMdClose />
+          </button>
+        </div>
+        <p className="text-lg">Selected entry: {framedata.input}</p>
+        <form
+          className={`grid [&_input]:text-secondaryText [&_textarea]:text-secondaryText [&_input]:p-1 [&_textarea]:p-1 [&_input]:rounded [&_textarea]:rounded`}
+          onSubmit={modifyFramedata}
+        >
+          <label htmlFor="input">Input</label>
+          <input
+            type="text"
+            name="input"
+            id="input"
+            placeholder={`"d/f+2,1" etc...`}
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
 
-        <label htmlFor="input">Hit Level</label>
-        <input
-          type="text"
-          name="hit_level"
-          id="hit_level"
-          defaultValue={framedata.hit_level}
-        />
+          <label htmlFor="input">Hit Level</label>
+          <input
+            type="text"
+            name="hit-level"
+            id="hit-level"
+            placeholder={`"m,h,l" etc...`}
+            value={hitLevel}
+            onChange={(e) => setHitLevel(e.target.value)}
+          />
 
-        <label htmlFor="input">Damage</label>
-        <input
-          type="text"
-          name="damage"
-          id="damage"
-          defaultValue={framedata.damage}
-        />
+          <label htmlFor="input">Damage</label>
+          <input
+            type="text"
+            name="damage"
+            id="damage"
+            placeholder="Start typing..."
+            value={damage}
+            onChange={(e) => setDamage(e.target.value)}
+          />
 
-        <label htmlFor="input">Startup</label>
-        <input
-          type="text"
-          name="startup"
-          id="startup"
-          defaultValue={framedata.startup}
-        />
+          <label htmlFor="input">Startup</label>
+          <input
+            type="text"
+            name="startup"
+            id="startup"
+            placeholder="Start typing..."
+            value={startup}
+            onChange={(e) => setStartup(e.target.value)}
+          />
 
-        <label htmlFor="input">Hit</label>
-        <input type="text" name="hit" id="hit" defaultValue={framedata.hit} />
+          <label htmlFor="input">Hit</label>
+          <input
+            type="text"
+            name="hit"
+            id="hit"
+            placeholder="Start typing..."
+            value={hit}
+            onChange={(e) => setHit(e.target.value)}
+          />
 
-        <label htmlFor="input">Block</label>
-        <input
-          type="text"
-          name="block"
-          id="block"
-          defaultValue={framedata.block}
-        />
+          <label htmlFor="input">Block</label>
+          <input
+            type="text"
+            name="block"
+            id="block"
+            placeholder="Start typing..."
+            value={block}
+            onChange={(e) => setBlock(e.target.value)}
+          />
 
-        <label htmlFor="input">Counter</label>
-        <input
-          type="text"
-          name="counter"
-          id="counter"
-          defaultValue={framedata.counter}
-        />
+          <label htmlFor="input">Counter</label>
+          <input
+            type="text"
+            name="counter"
+            id="counter"
+            placeholder="Start typing..."
+            value={counter}
+            onChange={(e) => setCounter(e.target.value)}
+          />
 
-        <label htmlFor="input">Notes</label>
-        <textarea name="input" id="input" defaultValue={framedata.input} />
-      </form>
+          <label htmlFor="input">Notes</label>
+          <textarea
+            name="input"
+            id="input"
+            placeholder="Start typing..."
+            value={notes.join("\n")}
+            onChange={handleNoteChange}
+          />
+
+          <br />
+
+          <button
+            type="submit"
+            className="bg-green-400 hover:bg-green-500 text-black p-1 rounded"
+            disabled={isLoading}
+          >
+            Submit
+          </button>
+        </form>
+      </div>
     </FullScreenOverlay>
   );
 }
