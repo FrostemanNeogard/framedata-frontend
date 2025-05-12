c6e1965cf45e2ef4bc6acf8609fe45b1a0ee4a76
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2025-05-12 22:14:47 +0200

    Improve styling

--- src/components/FullScreenOverlay.tsx
+++ src/components/FullScreenOverlay.tsx
@@ -1,6 +1,12 @@
 import { PropsWithChildren, useEffect } from "react";
 
-export default function FullScreenOverlay({ children }: PropsWithChildren) {
+type FullScreenOverlayProps = {
+  overrideDefault?: boolean;
+};
+export default function FullScreenOverlay({
+  children,
+  overrideDefault,
+}: PropsWithChildren<FullScreenOverlayProps>) {
   useEffect(() => {
     document.body.style.overflow = "hidden";
 
@@ -13,7 +19,11 @@ export default function FullScreenOverlay({ children }: PropsWithChildren) {
     <div
       className={`left-0 top-0 text-primaryText fixed w-screen h-screen bg-black/50 z-50 flex`}
     >
-      <div className="bg-primary h-3/5 w-2/3 m-auto p-5">{children}</div>
+      {overrideDefault ? (
+        children
+      ) : (
+        <div className="bg-primary min-h-3/5 w-2/3 m-auto p-5">{children}</div>
+      )}
     </div>
   );
 }
